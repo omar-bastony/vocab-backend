f941dbae15db8d2ec17fd3f3eab13ce302f8cd07
omar-bastony <bastonyomar@gmail.com>
2026-03-27 14:01:02 +0100

    fix image fit

--- server.js
+++ server.js
@@ -242,7 +242,7 @@ app.get('/api/image', async (req, res) => {
         const data = await response.json();
 
         if (data.results && data.results.length > 0) {
-            const imageData = { imageUrl: data.results[0].urls.small };
+            const imageData = { imageUrl: data.results[0].urls.regular };
             
             // NEW: Save image result to Redis
             try { await redis.set(imgCacheKey, imageData); } catch (e) {}
